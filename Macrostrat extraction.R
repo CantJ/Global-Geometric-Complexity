f1374c68d4983cb6266afd71f98c8758853941ea
James Cant <james.cant91@gmail.com>
2022-04-12 09:44:49 +0100

    Update Macrostrat extraction.R

--- Macrostrat extraction.R
+++ Macrostrat extraction.R
@@ -21,7 +21,7 @@ library(httr)
 # Data from Macrostrat is downloaded through an Application Programming Interface (API).
 # First determine the different bedrock classifications present within the database.
 strat_types <- read.csv("https://macrostrat.org/api/v2/defs/strat_names?all&format=csv")
-num_strat <- dim(strat_types)[1] # how many classification is this?
+num_strat <- dim(strat_types)[1] # how many classifications is this?
 
 # Define the permanent sections of URL for downloading data for each bedrock class - the Macrostrat API allows these different files to be called simultaneously. 
 # However the file size is too large to call in a single URL so it is nessecary to divide the calls.
@@ -51,19 +51,19 @@ download_func <- function(ii){
 ###################################################
 
 # run download
-Strat_data <- sapply(1:100, download_func, simplify = TRUE)
+Strat_data <- sapply(1:num_strat, download_func, simplify = TRUE)
 
 ###################################################
 # STEP 4: Save the extracted file
 ###################################################
 
 # Convert file into a single shapefile
-
+sf_data <- do.call(rbind, Strat_data)
 
 # Specify destination
 fileDest <- "/Volumes/Pocillopora/Geodiversity Data/Raw/Macrostrat/"
 
 # write to file
-st_write(sf_map, paste0(fileDest, "Macrostrat.shp"))
+suppressWarnings(write_sf(sf_data, paste0(fileDest, "Macrostrat.shp")))
 
 # ------------------------------------------------------ End of Code -------------------------------------------------
