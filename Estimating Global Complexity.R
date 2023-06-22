080c6083a5776b1f570e8e234d15fb617cd90c0a
James Cant <james.cant91@gmail.com>
2023-06-22 09:29:07 +0100

    Updated repository
    Removed old scripts and refined scripts used for updated analyses

---
+++ Estimating Global Complexity.R
@@ -0,0 +1,273 @@
+# This script is for estimating fractal dimension, surface rugosity, and height range from a global digital elevation model (Topography and Bathymetry combined), 
+# at a resolution of 248*248m. 
+# The outcome of this script is a large csv file (stored as a compressed parquet to allow for memory efficient storage).
+
+# Date last modified: May 2023
+# Primary Author: James Cant
+# ------------------------------------------------------------------------------------------------------------
+
+# Clear workspace
+rm(list=ls(all=TRUE))
+
+# Define directory pathways
+filePath <- 'E:/Geodiversity_Data/Finalised/Complexity v Biodiversity/'
+fileSave <- 'D:/Geodiversity/ComplexityFiles/' # temporary file directory for saving .csv sub-files
+# Define selected raster file
+SelectDEM <- 'Bathymetry_Eckert_248.vrt'# Global raster (248m resolution)
+# This file is a global DEM comprising terrestrial topography and ocean bathymetry. 
+# This file was produced by mosaicing tiles obtained from GEBCO.
+# This DEM was then reprojected to an Eckert IV equal area projection.
+# SelectDEM <- 'Topography_Eckert_23.vrt' # terrestrial raster (23m resolution)
+# This file has been produced by mosaicing together a series of GEOTIFF tiles obtained from the NASADEM program.
+# Further details on the product can be found at: https://earthdata.nasa.gov/esds/competitive-programs/measures/nasadem
+# The full DEM was then reprojected to an Eckert IV equal area projection.
+LandMask <- 'LandMask_Eckert_1km.tif' # This is a raster template denoting pixels associated with terrestrial and marine environments.
+# This file has been generated using from the Copernicus CCL Land Cover project using a 2020 map of land cover. Its has been projected to an Eckert 4 1240m projection.
+
+# load necessary packages
+library(rgdal)
+library(sp)
+library(raster)
+library(parallel)
+library(doParallel)
+library(terra)
+library(dplyr)
+library(readr)
+library(data.table)
+library(arrow)
+library(tidyr)
+
+#################################################
+# STEP 1: Define complexity function
+#################################################
+# *** This function has been adapted from Torres-Pulliza et al. (2020) Nature Ecol. & Evol 4. 1495-1501 ***
+
+# Estimate complexity values for a selected Longitudinal Band
+ExtractComplexity <- function(ii, DEMBand, L, L0, GridY, x0, progress = FALSE){
+  
+  # progress read out if requested (good for troubleshooting). Defaults to no progress display.
+  if(progress == TRUE) { print(ii) }
+  
+  # Isolate Grid Cell from Longitudinal DEM band provided.
+  GridCell <- crop(DEMBand, extent(x0, x0+L, GridY[ii], GridY[ii]+L))
+  
+  # Estimate Height range for selected DEM cell
+  H <- max(GridCell[!is.na(GridCell)]) - min(GridCell[!is.na(GridCell)])
+  
+  # Generate Spatial Grid data frame
+  TmpGridCell <- as(GridCell, 'SpatialGridDataFrame')
+  
+  # Estimate Rugosity
+  R <- surfaceArea(TmpGridCell) / L^2
+  
+  # Estimate Fractal Dimension
+  D <- suppressWarnings(3 - log10(H / (sqrt(2) * L0 * sqrt(R^2 - 1))) / log10(L / L0))
+  
+  # A little fix for grid cells that contain no height range
+  # If the selected grid cell contains no height variation (i.e. H and H0 == 0) 
+  # then it violates the theory of fractal dimension (because it is not fractal)
+  # and thus cannot be processed.
+  if(H == 0){
+    R <- 1
+    H <- 0
+    D <- 2
+  }
+  
+  # A further little fix incase the selected Grid Cell contains NA's
+  if(is.na(max(values(GridCell))) & is.na(min(values(GridCell)))){
+    R <- NA
+    H <- NA
+    D <- NA
+  }
+    
+  # Return extracted complexity values
+  return(list(Y = ii, Lat = GridY[ii], H = H, R = R, D = D))
+}
+
+#################################################
+# STEP 2: Determine scope/extent, scales of variation and resolution parameters
+#################################################
+
+# load DEM raster
+GlobalDEM <- vrt(paste0(filePath, SelectDEM))
+# check projection
+GlobalDEM # should be Eckert 4 equal area
+
+# define L0 (minimum resolution)
+L0 <- res(GlobalDEM)[1]
+
+# define L
+# Previous approaches have ensured the difference between L0 and L covers 2 orders of magnitude.
+# However a sensitivity test performed previously indicates that estimates of complexity will remain consistent at ~0.7 orders of magnitude
+# Therefore the scales selected will allow for calculating complexity at close to a 1km scale.
+L <- L0*5
+
+# define dimensions for sub-setting DEM in to manageable longitudinal bands equal to L in width to ease processing demands.
+minX <- ext(GlobalDEM)[1]
+maxX <- ext(GlobalDEM)[2] - L
+minY <- ext(GlobalDEM)[3]
+maxY <- ext(GlobalDEM)[4] - L
+
+# define longitudinal band sequence
+GridX <- seq(minX, maxX, by = L)
+# and define latitudinal indexing sequence
+# This index works by dividing each longitudinal band into latitudinal cells each L*L in dimension
+# remembering that L is equal to 5 pixels.
+GridY <- seq(minY, maxY, by = L)
+
+#################################################
+# STEP 3: Estimate complexity
+#################################################
+
+# Open multicore interface
+nCores = detectCores()
+cl <- makeCluster(nCores*0.6) # not to overload the computer system
+registerDoParallel(cl)
+
+# Run multicore processing
+# loop through XY grid extracting complexity measures
+initial_x <- length(list.files(fileSave, 'ComplexityValues'))+1 # allows for quick indexing following processing errors
+# Run process across cluster
+ComplexValues <- foreach(xx = initial_x:length(GridX), .inorder = FALSE) %dopar% { # cluster processing along longitudinal grid coordinates
+  
+  # load required packages across cluster
+  library(sp)
+  library(raster)
+  library(terra)
+  library(data.table)
+  library(readr)
+    
+  # Allow all cluster nodes access to the necessary rasters (rasters cannot be called from the global environment)
+  DEM <- vrt(paste0(filePath, SelectDEM))  
+  MaskRast <- rast(paste0(filePath, LandMask)) # Load Mask raster
+   
+  # define origin coordinate for selected longitudinal band
+  x0 <- GridX[xx]
+
+  # Clip rasters to focus on selected longitudinal band
+  tmpDEMRast <- raster(terra::crop(DEM, ext(x0, x0+L, minY, maxY+L)))
+  tmpMask <- terra::crop(MaskRast, ext(x0, x0+L, minY, maxY+L))
+  
+  # Extract complexity values
+  CellComplex <- as.data.frame(t(sapply(1:length(GridY), ExtractComplexity, DEMBand = tmpDEMRast, L = L, L0 = L0, GridY = GridY, x0 = x0)))
+  # transform out of list type
+  CellComplex[c('Y','Lat','H','R','D')] <- as.numeric(unlist(CellComplex[c('Y','Lat','H','R','D')]))
+  
+  # Identify the realm associated with each pixel
+  Realm <- numeric(dim(CellComplex)[1])
+  Realm[values(tmpMask) == 1] <- 1 # if pixel is terrestrial, value = 1
+  
+  # Attach Longitudinal details and realm identifier
+  XValues <- data.frame(X = rep(xx, dim(CellComplex)[1]),
+                        Lon = rep(x0, dim(CellComplex)[1]),
+                        Terrestrial = Realm) # Marine = 0 & Terrestrial = 1
+  CellComplex <- cbind(XValues, CellComplex)
+    
+  # and save outputs (in-case of system meltdowns)
+  fwrite(CellComplex, paste0(fileSave, 'ComplexityValues_X', xx, '.csv'), row.names = FALSE) # This will be one of 27292 csv subfiles.
+  
+  # return cluster output
+  CellComplex
+  # end of loop
+}
+
+# Close multicore cluster
+stopCluster(cl)
+
+# Save extracted complexities 
+# In-case of loop crashes and the .csv file subsets need to be called.
+# Compile the .csv sub file together
+ComplexValues <- as.data.frame(list.files(fileSave, full.names = TRUE) %>% 
+                               lapply(read_csv, show_col_types = FALSE) %>% 
+                               bind_rows)
+# and write to file (Checkpoint)
+# using the arrow package this large csv file is saved in a memory efficient format
+write_parquet(ComplexValues, paste0(filePath,'GlobalComplexity.parquet'))
+
+#################################################
+# STEP 4: Clean Complexity estimates
+#################################################
+
+# Reopen dataset using memory efficient format
+ComplexValues <- read_parquet(paste0(filePath,'GlobalComplexity.parquet'), as_data_frame = F)
+
+# check for inappropriate entries
+ComplexValues %>%
+  filter(is.infinite(H)) %>% collect() %>% dim()
+ComplexValues %>%
+  filter(is.infinite(R)) %>% collect() %>% dim() # no inf entries in rugosity & Height range
+ComplexValues %>%
+  filter(is.infinite(D)) %>% collect() %>% dim() # 7531 inf entries in Fractal dimension
+ComplexValues %>%
+  filter(!is.infinite(D) & D < 2 | D > 3) %>% collect() %>% dim() # a further 493 Fractal dimension values are outside the range of 2 to 3
+ComplexValues %>%
+  filter(!is.infinite(D) & R < 1) %>% collect() %>% dim() # No rugosity values are lower than 1.
+ComplexValues %>%
+  filter(!is.infinite(D) & H < 0) %>% collect() %>% dim() # Height range values are all positive.
+# carry out initial data cleaning
+ComplexValues |>
+  mutate(H = ifelse(is.infinite(D), NA, H),
+         R = ifelse(is.infinite(D), NA, R),
+         D = ifelse(is.infinite(D), NA, D)) |>
+  mutate(D = ifelse(D<2 | D>3, NA, D)) |>
+  collect() |>
+  write_parquet(paste0(filePath, 'GlobalComplexity.parquet')) # overwrite output to memory efficient object
+
+# Reload updated data file
+ComplexValues <- read_parquet(paste0(filePath,'GlobalComplexity.parquet'), as_data_frame = F)
+
+# Confirm that for all instances where D = 2, then R = 1 and H = 0 and visa-versa.
+ComplexValues %>%
+  filter(D == 2) %>% count(R, sort = TRUE) %>% collect() # When D = 2 then R = 1 in all cases.
+ComplexValues %>%
+  filter(D == 2) %>% count(H, sort = TRUE) %>% collect() # When D = 2 then H = 0 in all cases.
+ComplexValues %>%
+  filter(H == 0) %>% count(R, sort = TRUE) %>% collect() # When H = 0 then R = 1 in all cases.
+ComplexValues %>%
+  filter(H == 0) %>% count(D, sort = TRUE) %>% collect() # When H = 0 then D = 2 in all cases.
+ComplexValues %>%
+  filter(R == 1) %>% count(D, sort = TRUE) %>% collect()
+ComplexValues %>%
+  filter(R == 1) %>% count(H, sort = TRUE) %>% collect() # However there are 14702 instances where R = 1 but neither D = 2 nor H = 0.
+# These are likely rounding errors and need to be corrected.
+# Isolate offending cells. 
+RugosityErrors <- ComplexValues %>%
+  filter(R == 1 & H!=0) %>% 
+  collect %>%
+  tibble()
+# Allow R to cope with more digits to prevent values very close to 1 being rounded down.
+options(digits = 22) 
+# Run correction loop
+for (ii in 1:dim(RugosityErrors)[1]) {
+  # progress read out
+  print(ii)
+  # Extract grid cell corresponding with error
+  tmpRast <- crop(GlobalDEM, ext(RugosityErrors$Lon[ii], RugosityErrors$Lon[ii]+L, RugosityErrors$Lat[ii], RugosityErrors$Lat[ii]+L))
+  # calculate height range
+  H <- max(tmpRast[!is.na(tmpRast)]) - min(tmpRast[!is.na(tmpRast)])
+  RugosityErrors$H2[ii] <- H
+  # Calculate rugosity
+  TmpGridCell <- as(raster(tmpRast), 'SpatialGridDataFrame')
+  R <- surfaceArea(TmpGridCell) / L^2
+  RugosityErrors$R2[ii] <- R
+  # and calculate fractal dimension
+  RugosityErrors$D2[ii] <- suppressWarnings(3 - log10(H / (sqrt(2) * L0 * sqrt(R^2 - 1))) / log10(L / L0))
+}
+# Drop repeated variables that aren't needed for merging dataframes
+RugosityErrors <- RugosityErrors[,-(c('H','D','R','Terrestrial'))]
+# Insert the corrected values back into the original data file
+ComplexValues %>% 
+  left_join(RugosityErrors,  
+            by = c("X" = 'X', "Y" = 'Y', "Lon" = 'Lon', "Lat" = 'Lat')) %>%
+  mutate(R = ifelse(!is.na(R2), R2, R)) %>% 
+  select(X, Y, Lon, Lat,Terrestrial, H, R, D) %>%
+  write_parquet(paste0(filePath, 'GlobalComplexity.parquet')) # overwrite output to memory efficient object
+# This is the final output of this script.
+
+# Reload updated data file to confirm successful data cleaning.
+ComplexValues <- read_parquet(paste0(filePath,'GlobalComplexity.parquet'), as_data_frame = F)
+
+ComplexValues %>%
+  filter(R == 1) %>% count(D, sort = TRUE) %>% collect() # all good to proceed.
+
+##### ------------------------------------------------------------- End of Code --------------------------------
---
+++ Resolution Sensitivity.R
@@ -0,0 +1,347 @@
+# This script is for testing the accuracy of increasing map resolution on estimates of fractal dimension and rugosity
+# This script will also compare complexity estimate calculated using the planar equation and the surface area function
+#
+# Main author: James Cant (jic2@st-andrews.ac.uk)
+# Date Last Modified: May 2023
+# -----------------------------------------------------------
+
+# Clear workspace
+rm(list=ls(all=TRUE))
+
+# Define directory pathways
+filePath <- 'E:/Geodiversity_Data/Finalised/Complexity v Biodiversity/'
+# Define raster files
+TerrestrialDEM <- 'Topography_Eckert_23.vrt' # terrestrial raster (23m resolution)
+# This file has been produced by mosaicing together a series of GEOTIFF tiles obtained from the NASADEM program.
+# Further details on the product can be found at: https://earthdata.nasa.gov/esds/competitive-programs/measures/nasadem
+# The full DEM was then reprojected to an Eckert IV equal area projection.
+GlobalDEM <- 'Bathymetry_Eckert_248.vrt'# Bathymetry raster (248m resolution)
+# Despite the naming here, this file is actually a global DEM comprising terrestrial topography and ocean bathymetry. 
+# This file was produced by mosaicing tiles obtained from GEBCO.
+# This DEM was then reprojected to an Eckert IV equal area projection.
+
+# load necessary packages
+library(rgdal)
+library(sp)
+library(raster)
+library(terra)
+library(dplyr)
+library(SpaDES)
+library(parallel)
+library(data.table)
+library(ggplot2)
+library(viridis)
+
+#################################################
+# STEP 1: Define complexity functions
+#################################################
+# *** These functions have been adapted from Torres-Pulliza et al. (2020) Nature Ecol. & Evol 4. 1495-1501 ***
+# Importantly these different functions will allow for testing the association between Rugosity and fractal dimension 
+# and the accuracy of different approaches used to calculate them.
+
+# Rugosity function
+Rugosity <- function(H0, L0) {
+  R <- sqrt((H0^2) / (2 * L0^2) + 1)
+  # output
+  return(R)
+}
+
+# Height Range function for calculating height variation across different spatial windows
+estimateHR <- function(x, y, s, Rast, x0, y0) {
+  bx <- extent(cbind(c(x0 + x, y0 + y), c(x0 + x + s, y0 + y + s)))
+  # output
+  return(diff(range(getValues(crop(Rast, bx)), na.rm = TRUE)))
+}
+
+# Convert DEM into moving height range windows
+height_variation <- function(Rast, scl, L, y0, x0) {
+  # define storage output
+  temp <- data.frame()
+  # loop through each scale
+  for (s in scl) {
+    # define scale grid
+    inc <- seq(0, L - s, s)
+    x <- rep(inc, L/s)
+    y <- rep(inc, each = L/s)
+    # estimate height range for each grid cell
+    temp <- rbind(temp, data.frame(L0 = s, x = x, y = y, H0 = mcmapply(estimateHR, x = x, y = y, s = s, MoreArgs = list(Rast = Rast, x0 = x0, y0 = y0))))
+  }
+  # return output
+  return(temp)
+}
+
+# Estimate complexity values for the selected grid cell
+# This cannot be used for the Terrestrial data set due to the presence of 0 values along the margins of water bodies.
+# As zero values cannot be log transformed without an adjustment factor. However introducing an adjustment factor influences estimates of Fractal Dimension.
+ComplexityValues <- function(data, L, L0) {
+  # log10 transform
+  data$H0 <- log10(data$H0)
+  data$L0 <- log10(data$L0)
+  # A little fix for Height ranges of zero (if there are any)
+  if(dim(data[is.infinite(data$H0),])[1] > 0){ data[is.infinite(data$H0),]$H0 <- 0 }
+  
+  # Estimate mean height range across each spatial scale
+  data_m <- aggregate(H0 ~ L0, data, mean)
+  
+  # Extract height range at both L and L0
+  H <- (10^data_m$H0[data_m$L0 == log10(L)])
+  H0 <- (10^data_m$H0[data_m$L0 == log10(L0)])
+  
+  # Calculate slope coefficient (S)
+  mod <- lm(H0 ~ L0, data_m)
+  D <- 3 - coef(mod)[2] # D = 3 - S
+  
+  # Calculate rugosity
+  R <- Rugosity(H0, L0)		
+  
+  # A little fix for grid cells that contain no height range
+  # If the selected grid cell contains no height variation (i.e. H and H0 == 0) 
+  # then it violates the theory of fractal dimension (because it is not fractal)
+  # and thus cannot be processed.
+  if(H == 0 & H0 ==0){
+    R <- 1
+    H <- 0
+    D <- 2
+  }
+  
+  # outputs
+  return(list(D = D, R = R, H = H))
+}
+
+# Alternative method (Combining all complexity measures)
+# This function is for when log transformation is not possible (due to the presence of zeros)
+ExtractComplexity <- function(GridCell, L, L0){
+  # Estimate Height range for selected grid cell
+  H <- max(GridCell[!is.na(GridCell)]) - min(GridCell[!is.na(GridCell)])
+  
+  # Generate Spatial Grid data frame
+  TmpGridCell <- as(GridCell, 'SpatialGridDataFrame')
+  # Estimate Rugosity
+  R <- surfaceArea(TmpGridCell) / L^2
+  
+  # Estimate Fractal Dimension
+  D <- suppressWarnings(3 - log10(H / (sqrt(2) * L0 * sqrt(R^2 - 1))) / log10(L / L0))
+  
+  # A little fix for grid cells that contain no height range
+  # If the selected grid cell contains no height variation (i.e. H and H0 == 0) 
+  # then it violates the theory of fractal dimension (because it is not fractal)
+  # and thus cannot be processed.
+  if(H == 0){
+    R <- 1
+    H <- 0
+    D <- 2
+  }
+  
+  return(list(H = H, R = R, D = D))
+}
+
+#################################################
+# STEP 2: Implement the effect of changing resolution on complexity estimates
+#################################################
+
+# Define function to test the varying effects of changing L.
+# test both informs the function as to whether both the planar equation and surfaceArea function methods are to be compared.
+VaryingL <- function(ii, test.both = TRUE){
+  
+  # set step counter for progress monitoring
+  stepCount <- 1
+  
+  # Define scale over which to calculate height variation
+  scl <- seq(from = L0, to = L[ii], by = L0)
+  
+  # define upper grid dimensions (for sub-setting DEM in to manageable chunks equal to L*L in dimension for processing)
+  maxX <- Xlimit - L[ii]
+  maxY <- Ylimit - L[ii]
+  # and define Terrestrial and Marine grids
+  GridX <- seq(minX, maxX, by = L[ii])
+  GridY <- seq(minY, maxY, by = L[ii])
+  
+  # define storage
+  PixelValues <- data.frame()
+  
+  # Run for loop
+  for(xx in 1:length(GridX)){
+    for(yy in 1:length(GridY)) { # work through each grid cell to estimate its corresponding measures of complexity.
+      # update step count
+      cat(ii, '.', stepCount, '\n')
+      stepCount <- stepCount+1
+      
+      # define origin coordinates for selected grid cell
+      x0 <- GridX[xx]
+      y0 <- GridY[yy]
+      
+      # Clip raster to focus on selected grid cell (L*L)
+      tmpRast <- raster(terra::crop(DEM, ext(x0, x0+L[ii], y0, y0+L[ii])))
+      
+      if(test.both == TRUE) {
+        # For each grid cell calculate complexity using the two approaches
+        # Approach 1: Calculate Height range and rugosity using the plane-equations.
+        # This portion of the code is the processing time bottleneck.
+        cellHR <- height_variation(Rast = tmpRast, scl = scl, L = L[ii], x0 = x0, y0 = y0)
+      
+        # Extract complexity values
+        CellComplex1 <- ComplexityValues(data = cellHR, L = L[ii], L0 = L0)
+        # Approach 2: Calculate Height range and rugosity using inbuilt R functions.
+        CellComplex2 <- ExtractComplexity(GridCell = tmpRast, L = L[ii], L0 = L0)
+      
+        # Save outputs
+        Outputs <- c(L[ii], # scale
+                     xx, # Grid index values
+                     yy, 
+                     x0, # Longitude
+                     y0, # Latitude
+                     as.numeric(CellComplex1$H),
+                     as.numeric(CellComplex1$R),
+                     as.numeric(CellComplex1$D),
+                     as.numeric(CellComplex2$H),
+                     as.numeric(CellComplex2$R),
+                     as.numeric(CellComplex2$D))
+      
+        # And store
+        PixelValues <- rbind(PixelValues, Outputs)
+        colnames(PixelValues) <- c('L','X','Y','Lon','Lat','H_theory','R_theory','D_theory','H','R','D')
+      } else {
+        # For each grid cell calculate complexity using only in the inbuilt R functions.
+        CellComplex1 <- ExtractComplexity(GridCell = tmpRast, L = L[ii], L0 = L0)
+        
+        # Save outputs
+        Outputs <- c(L[ii], # scale
+                     xx, # Grid index values
+                     yy, 
+                     x0, # Longitude
+                     y0, # Latitude
+                     as.numeric(CellComplex1$H),
+                     as.numeric(CellComplex1$R),
+                     as.numeric(CellComplex1$D))
+        
+        # And store
+        PixelValues <- rbind(PixelValues, Outputs)
+        colnames(PixelValues) <- c('L','X','Y','Lon','Lat','H','R','D')
+      }
+    }      
+  }    
+  
+  # and return
+  return(PixelValues)
+}
+  
+# load Global DEM raster
+DEM <- vrt(paste0(filePath, GlobalDEM))
+
+# define L0 (minimum resolution)
+# Minimum resolution requires 2*2 grid cell
+L0 <- res(DEM)[1]*2
+
+# The code below will work on only a subsection of the global DEM to test the effect of changing resolutions on complexity estimates
+# This subsection is equal to ~9 cells at a spatial scale 2 orders of magnitude larger than L0.
+# Define largest spatial scale
+Lmax <- L0*(10^2) 
+# Define boundaries of map subsample.
+minX <- 3.5e+06; minY <- 0 
+Xlimit <- minX+(Lmax*3); Ylimit <- minY+(Lmax*3)
+# Outline scale over which to vary L
+L <- c(L0*c(2,5,10^1,10^2))
+  
+# Test how varying L between two and less than one order of magnitude (49km and 1km) influences complexity estimates
+LTest <- lapply(4:1, VaryingL)
+# Convert outputs into a data frame
+LTestdf <- rbindlist(LTest)
+# and save data as a checkpoint
+write.csv(LTestdf, paste0(filePath, 'ResolutionSens.csv'), row.names = FALSE)
+
+#################################################
+# STEP 3: Test the effect of changing resolution on complexity estimates
+#################################################
+
+# Load data from checkpoint
+LTestdf <- read.csv(paste0(filePath, 'ResolutionSens.csv'))
+
+# Firstly, it appears using the planar equation returns incorrect estimates of fractal dimension (i.e. D < 2)
+# Visualize how the complexity estimates calculated using the two approaches compare to one another.
+# Patterns in Rugosity
+ggplot(LTestdf, aes(x=R_theory, y=R, col = L)) +
+  geom_point(size = 2) +
+  xlab('\nR (Equation)') +
+  ylab('R (R function)\n') +
+  scale_color_gradientn(colours = viridis(n = 4, option = 'cividis'),
+                        guide = guide_colorbar(ticks = F, title = 'Scale (L)',
+                                               reverse = F, label = T,
+                                               na.value = "white")) +
+  theme_bw() + theme(panel.grid.major = element_blank(),
+                     panel.grid.minor = element_blank(),
+                     axis.title = element_text(size = 15, colour = 'black'),
+                     axis.text.x = element_text(size = 15, colour = "black"), axis.text.y = element_text(size = 15, colour = "black"),
+                     legend.justification=c(0,0), legend.position=c(0.05,0.6), 
+                     legend.background = element_blank(),
+                     legend.box.background = element_rect(colour = "black"))
+
+# Patterns in Height Range
+ggplot(LTestdf, aes(x=H_theory, y=H, col = L)) +
+  geom_point(size = 2) +
+  xlab('\nH (Equation)') +
+  ylab('H (R function)\n') +
+  scale_color_gradientn(colours = viridis(n = 4, option = 'cividis'),
+                        guide = guide_colorbar(ticks = F, title = 'Scale (L)',
+                                               reverse = F, label = T,
+                                               na.value = "white")) +
+  theme_bw() + theme(panel.grid.major = element_blank(),
+                     panel.grid.minor = element_blank(),
+                     axis.title = element_text(size = 15, colour = 'black'),
+                     axis.text.x = element_text(size = 15, colour = "black"), axis.text.y = element_text(size = 15, colour = "black"),
+                     legend.justification=c(0,0), legend.position=c(0.05,0.6), 
+                     legend.background = element_blank(),
+                     legend.box.background = element_rect(colour = "black"))
+
+# Patterns in Fractal Dimension
+ggplot(LTestdf, aes(x=D_theory, y=D, col = L)) +
+  geom_point(size = 2) +
+  xlab('\nD (Equation)') +
+  ylab('D (R function)\n') +
+  scale_color_gradientn(colours = viridis(n = 4, option = 'cividis'),
+                        guide = guide_colorbar(ticks = F, title = 'Scale (L)',
+                                               reverse = F, label = T,
+                                               na.value = "white")) +
+  theme_bw() + theme(panel.grid.major = element_blank(),
+                     panel.grid.minor = element_blank(),
+                     axis.title = element_text(size = 15, colour = 'black'),
+                     axis.text.x = element_text(size = 15, colour = "black"), axis.text.y = element_text(size = 15, colour = "black"),
+                     legend.justification=c(0,0), legend.position=c(0.05,0.6), 
+                     legend.background = element_blank(),
+                     legend.box.background = element_rect(colour = "black"))
+# Patterns are largely consistent across scales
+
+# Rugosity and Fractal Dimension are additive values and so regardless of the scale used the mean values of each should correspond for a given area.
+aggregate(LTestdf[,c('H_theory','R_theory','D_theory','H','R','D')], list(LTestdf$L), mean)
+# The approach using the plane equation does not appear to reflect this. However the approach using the inbuilt R functions does, with the R and D values at 0.5, 1, and 2 orders of magnitude reflecting similar scales of complexity
+
+# ------------------------------------------
+# Given that all further analyses will now focus on using the inbuilt R functions to calculate complexity the value of L0 can be set as equal to a single DEM pixel.
+# It is therefore necessary to test how this effects the influence of resolution on estimates of complexity
+
+# Re-define L0 (minimum resolution)
+L0 <- res(DEM)[1]
+# Re-define largest spatial scale
+Lmax <- L0*(10^2) 
+# Re-outline scale over which to vary L
+L <- c(L0*c(2,5,10^1,10^2))
+
+# Test how varying L between two and one order of magnitude (25km and 0.5km) influences complexity estimates
+LTest2 <- lapply(4:1, VaryingL, test.both = FALSE) # this analysis will only focus on the approach using the inbuilt R surfaceArea function to calculate R and D.
+# Convert outputs into a data frame
+LTestdf2 <- rbindlist(LTest2)
+# and save data as a checkpoint
+write.csv(LTestdf2, paste0(filePath, 'ResolutionSens2.csv'), row.names = FALSE)
+
+# Load data from checkpoint
+LTestdf2 <- read.csv(paste0(filePath, 'ResolutionSens2.csv'))
+
+# Rugosity and Fractal Dimension are additive values and so regardless of the scale used the mean values of each should correspond for a given area.
+aggregate(LTestdf2[,c('H','R','D')], list(LTestdf2$L), mean)
+# Additive estimates of rugosity and fractal dimension remain largely consistent down to a scale of 1240m (~km).
+
+# Confirm variable distributions
+range(LTestdf2$H)
+range(LTestdf2$R)
+range(LTestdf2$D) # A couple of pixels with values higher than expected.
+
+#--------------------------------------------------------------- End of Code ------------------------------
